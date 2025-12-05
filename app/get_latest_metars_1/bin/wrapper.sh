8d35b1c919de4810e277ed103de25904aafbb305
Andre Lanyon <andre.lanyon@metoffice.gov.uk>
2025-12-05 13:04:56 +0000

    First commit

---
+++ app/get_latest_metars_1/bin/wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+
+# Activate cloned sss environment
+conda activate default_clone_nov_2025
+
+# Run first guess taf code
+python ${CODE_DIR}/master/get_latest_metars.py
+
+# Deactivate environment
+conda deactivate
